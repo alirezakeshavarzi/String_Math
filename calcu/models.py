d5ff99f7a17ab30aefd71e8ed6b96f8359269725
Alireza.k <ahmidyly@gmail.com>
2024-12-15 23:05:34 +0330

    add User model for db

--- calcu/models.py
+++ calcu/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+class User(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    email_ = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    membership_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email_
 # Create your models here.
