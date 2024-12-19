0f380787f760d9ea685021ac7a0de7353071756f
Alireza.k <ahmidyly@gmail.com>
2024-12-19 22:51:23 +0330

    for bug fixed

--- account/models.py
+++ account/models.py
@@ -6,9 +6,21 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     email_n = models.EmailField(unique=True)
-    #password = models.CharField(max_length=100)
     membership_date = models.DateTimeField(auto_now_add=True)
+
+    # Avoid conflicts with the default `auth.User` relationships
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="custom_user_groups",  # Unique related name
+        blank=True,
+        help_text="The groups this user belongs to.",
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="custom_user_permissions",  # Unique related name
+        blank=True,
+        help_text="Specific permissions for this user.",
+    )
 
     def __str__(self):
         return self.email_n
-# Create your models here.
