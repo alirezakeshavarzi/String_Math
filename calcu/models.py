949bf847afe7433aa57d299237b72a8b0ef72796
Alireza.k <ahmidyly@gmail.com>
2025-02-12 11:57:44 +0330

    add username

--- calcu/models.py
+++ calcu/models.py
@@ -1,13 +1,13 @@
-from django.db import models
-
-
-class User(models.Model):
-    first_name = models.CharField(max_length=40)
-    last_name = models.CharField(max_length=40)
-    email_n = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    membership_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.email_n
-# Create your models here.
+# from django.db import models
+#
+#
+# class User(models.Model):
+#     first_name = models.CharField(max_length=40)
+#     last_name = models.CharField(max_length=40)
+#     email_n = models.EmailField(unique=True)
+#     password = models.CharField(max_length=100)
+#     membership_date = models.DateTimeField(auto_now_add=True)
+#
+#     def __str__(self):
+#         return self.email_n
+# # Create your models here.
