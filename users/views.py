e5bc5ca6b4c7b093c11f9ce1fb475313965409ad
Alireza.k <ahmidyly@gmail.com>
2025-02-20 17:03:22 +0330

    bug fixed

--- users/views.py
+++ users/views.py
@@ -1,50 +1,41 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from .forms import LoginForms
+from django.contrib.auth import authenticate, login
 
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework_simplejwt.tokens import RefreshToken
 
+def login_signup(reqeust):
+    return render(reqeust, "signup_login.html")
 
 
-# I create a function for login and signup
 def login_view(request):
-    if request.method == "post":
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
 
-        username = request.data.get('username')
-        password = request.data.get('password')
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
 
-        user = authenticate(username=username, password=password)
-
-        if user is not None:
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-                'username': user.username
-            })
-        else:
-            return Response({"error" : 'Invalid credentials'}, status=400)
-
-        # when from type is login do this...
-        if form_type == "login":
-            user = authenticate(username=username, password=password)
-            # what is in user
-            print("user /////////////// : ", user)
+            user = authenticate(request, username=username, password=password)
+            print("This is user ///////////////////////// /////////// : ", user)
 
             if user:
                 login(request, user)
 
-                # redirect to home page (main page)
-                return redirect('home')
-            else:
-                return render(request, 'signup_login.html', {'error': 'Invalid credentials'})
-        return render("index.html")
+                refresh = RefreshToken.for_user(user)
+                print("refresh //////////////////// : /////////// ", refresh)
 
 
-def signup_login(req):
-    return render(req,'signup_login.html')
+                request.session['access_token'] = str(refresh.access_token)
+                request.session['refresh_token'] = str(refresh)
 
-# Create your views here.
+                return render(request, 'index.html', {"msg" : "welcome"})
 
+
+def protected_view(request):
+    access_token = request.session.get('access_token')
+
+    if not access_token:
+        return redirect('auth')
+
+    return render(request, 'index.html', {"msg" : "welcome"})
