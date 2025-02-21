cddb52f5b778377e16e298e24bd2a48a9b60e23c
Alireza.k <ahmidyly@gmail.com>
2025-02-21 14:23:28 +0330

    create signup form in django

--- users/forms.py
+++ users/forms.py
@@ -5,3 +5,9 @@ from .models import User
 class LoginForms(forms.Form):
     username = forms.CharField(max_length=80)
     password = forms.CharField(widget=forms.PasswordInput)
+
+class SignupForms(forms.Form):
+    username = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    conf_password = forms.CharField(widget=forms.PasswordInput)
