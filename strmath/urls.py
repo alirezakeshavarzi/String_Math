5dfa2886147af4902dee1baad5b61e5e603107d3
Alireza.k <ahmidyly@gmail.com>
2025-02-23 12:54:51 +0330

    add forget password view from django

--- strmath/urls.py
+++ strmath/urls.py
@@ -1,19 +1,6 @@
-"""strmath URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from calcu.views import index
@@ -41,6 +28,25 @@ urlpatterns = [
 
     path('update_password/', update_password, name='updatepass'),
 
+
+    # Forget Password
+    # صفحه درخواست بازنشانی رمز عبور
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+         name="reset_password"),
+
+    # صفحه‌ای که می‌گوید ایمیل بازیابی ارسال شد
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"),
+         name="password_reset_done"),
+
+    # صفحه‌ای که کاربر از طریق لینک ایمیل به آن هدایت می‌شود
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
+         name="password_reset_confirm"),
+
+    # صفحه‌ای که پس از تغییر موفقیت‌آمیز رمز عبور نمایش داده می‌شود
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
+         name="password_reset_complete"),
 
     # 4. add path from simple jwt
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
